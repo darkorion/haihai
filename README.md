4cb856d8249ab5065aaf36a95f2ad55b42cbeb1e
Tewi Inaba <fuwaneko@haineko.ru>
2012-12-27 16:57:47 +0900

    Command line arguments

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-﻿はいはい (haihai)
+﻿﻿はいはい (haihai)
 ======
 
 Script to convert H264 Hi10P MKV to HiP MKV preserving original MKV content (except for original video track of course).
+
+Discussion
+======
+
+[On Google+](https://plus.google.com/107252355899172817272)
+
+Issues
+======
+
+Script assumes that video track in MKV has index of 0. Will be fixed soon.
 
 Installation
 ======
@@ -13,6 +23,29 @@ Installation
 5. Put some Hi10P files into 10bit subfolder
 6. Run haihai.py
 
+Fuck this, I want binaries!
+======
+
+No problem, get it [here](http://konousa.ru/10to8.7z).
+
+1. Put videos into 10bit folder (create it)
+2. Run haihai.exe and you're good
+
+Linux?
+======
+
+Soon. For now you can use Wine — it works.
+
+Advanced
+======
+
+You can put Hi10P in subfolders and script will preserve them. That's what it was created for :)
+
+Gui?
+======
+
+No. This is batch processing script made to be as autonomous as possible (run and forget). Gui is not needed.
+
 Options
 ======
 
@@ -23,4 +56,4 @@ Install py2exe for python and generate haihai.exe for use w/o Python via setup.p
 What's with the name?
 ======
 
-Roughly Japanese “yesyes”, comes from **Hi**10P + **Hi**P
+Roughly Japanese “yesyes”, comes from **Hi**10P + **Hi**P.
--- haihai.py
+++ haihai.py
@@ -1,9 +1,13 @@
-﻿'''
+﻿#!/usr/bin/python
+#coding=utf-8
+
+'''
 This is haihai.py, a script to transcode Hi10P MKV videos to HiP suitable for hardware players.
 '''
 import subprocess
 import os
 import copy
+import argparse
 
 __author__ = "Tewi Inaba"
 __copyright__ = "2012"
@@ -18,16 +22,22 @@ __status__ = "Development"
 # list of two lists: indexes of substituted arguments, arguments itself
 # extract video track from mkv, assumes that there is only one track and it has index of 0
 extract_cmd = [[2], ["mkvtoolnix/mkvextract.exe", "tracks", "%s", "0:video.264"]]
-# encodes video track using 8-bit x264.exe, you can switch it with 10-bit exe and get symmetrical script that will re-encode 8-bit to 10-bit :)
-encode_cmd = [[8], ['x264.exe', '--preset', 'fast', '--tune', 'animation', '--crf', '20', '--fps', '%s', '-o', 'video.8bit.264', 'video.264']]
-# merges re-encoded video track into source mkv, removing old one
+# encodes video track using 8-bit x264.exe, you can switch it with 10-bit exe and get symmetrical script that will transcode 8-bit to 10-bit :)
+encode_cmd = [[8], ['x264.exe', '--preset', '%s', '--tune', '%s', '--crf', '%i', '--fps', '%s', '-o', 'video.8bit.264', 'video.264']]
+# merges transcoded video track into source mkv, removing old one
 merge_cmd = [[2, 5], ['mkvtoolnix/mkvmerge.exe', '-o', '%s', '-d', '!0', '%s', 'video.8bit.264']]
 # gets video track fps, assumptions are the same as in extract_cmd
 getfps_cmd = [[1], ["mkvtoolnix/mkvfps.exe", "%s"]]
 
 
-# runs external system command with parameters and without spawning window on Windows OSes
 def run_cmd(cmd, params):
+    '''
+        Runs external system command with parameters and without spawning window on Windows OSes
+        cmd shoud be list of two lists:
+         - first list contains indexes of substituted arguments
+         - second list contains list of arguments
+        params is a list of parameters that will be used as substitutions
+    '''
     command = copy.deepcopy(cmd)
     j = 0
     for i in command[0]:
@@ -45,8 +55,12 @@ def run_cmd(cmd, params):
     result = process.communicate()[0]
     return result
 
-# main function that does recursive folder search
-def job(main_dir, d):
+def job(main_dir, d = None):
+    '''
+        Does recursive folder search, finds all MKV files and processes them
+        main_dir - a dir where to start search
+        d - None on first call, or any subfolder to limit recursion there
+    '''
     if d is not None:
         work_dir = os.path.join(main_dir, d)
     else:
@@ -67,10 +81,13 @@ def job(main_dir, d):
         else:
             job_file(main_dir, d, item)
 
-# function that re-encodes single file
-# args: high-level folder, current-level folder, base file name
-# example args: 10bit, chuunibyou, chuunibyou-01.mkv
 def job_file(main_dir, d, f):
+    '''
+        Transcodes a single file
+        main_dir - see job
+        d - see job
+        f - base file name
+    '''
     if d is None:
         work_file = os.path.join(main_dir, f)
         output_file = os.path.join("8bit", f)
@@ -102,12 +119,38 @@ def job_file(main_dir, d, f):
     print "Merging"
     run_cmd(merge_cmd, [output_file, work_file])
 
-# I put my Hi10P into 10bit folder, you can use whatever you want, even absolute path
-# BUT: output ALWAYS goes into 8bit subfolder near this script
-job("10bit", None)
+def main():
+    '''
+        An example of transcoding using this module
+        
+        I put my Hi10P into 10bit folder, you can use whatever you want, even absolute path
+        BUT: output ALWAYS goes into 8bit subfolder near this script
+    '''
 
-# delete leftovers
-if os.path.exists("video.264"):
-    os.remove("video.264")
-if os.path.exists("video.8bit.264"):
-    os.remove("video.8bit.264")
+    # parse arguments
+    parser = argparse.ArgumentParser(description='Transcode Hi10P to HiP.')
+    parser.add_argument('--crf', metavar='crf', type=int, default=20,
+                       help='basic quality parameter, lower is better, 24 considered default by x264, 20 is default in haihai')
+    parser.add_argument('--tune', dest='tune', type=str, default="animation",
+                       help='tune x264 for specific content, default is animation, see x264 help for more')
+    parser.add_argument('--preset', dest='preset', type=str, default="fast",
+                       help='x264 preset, default is fast, for low-quality source use medium or slow')
+
+    args = parser.parse_args()
+
+    # build encode command based on arguments
+    encode_cmd[1][2] = encode_cmd[1][2] % args.preset
+    encode_cmd[1][4] = encode_cmd[1][4] % args.tune
+    encode_cmd[1][6] = encode_cmd[1][6] % args.crf
+
+    job("10bit", None)
+
+    # delete leftovers
+    if os.path.exists("video.264"):
+        os.remove("video.264")
+    if os.path.exists("video.8bit.264"):
+        os.remove("video.8bit.264")
+
+# you can import this module and call module functions from your scripts now
+if __name__ == "__main__":
+    main()
